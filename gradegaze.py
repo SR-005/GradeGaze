155109c8ee1152a1428608bdfd605159c221e1ae
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-11 22:02:38 +0530

    cleaning data

--- gradegaze.py
+++ gradegaze.py
@@ -5,5 +5,5 @@ import seaborn as sns
 
 
 df=pd.read_csv("student-mat.csv")
-dfdup=df.drop(["school","sex","address","famsize","Pstatus","Medu","Fedu","Mjob","Fedu","reason","guardian","schoolsup","famsup","paid","activities","nursery","higher","famrel","freetime","goout","Dalc","Walc","health","absences"],axis=1)
+dfdup=df.drop(["school","age","sex","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","Fedu","reason","guardian","schoolsup","famsup","paid","activities","nursery","higher","famrel","freetime","goout","Dalc","Walc","health","absences","romantic"],axis=1)
 print(dfdup.info())
