617b28f6e8ad21ca092f7428815ace63f102c2a5
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-12 21:52:15 +0530

    plotting graphs

--- gradegaze.py
+++ gradegaze.py
@@ -8,5 +8,6 @@ df=pd.read_csv("student-mat.csv")
 dfmain=df.drop(["school","age","sex","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","Fedu","reason","guardian","schoolsup","famsup","paid","activities","nursery","higher","famrel","freetime","goout","Dalc","Walc","health","absences","romantic"],axis=1)
 print(dfmain.info())
 
-sns.pairplot(dfmain, kind="scatter",plot_kws={"alpha":0.5})
+#sns.pairplot(dfmain, kind="hist",plot_kws={"alpha":0.5})
+sns.scatterplot(data=df,x="studytime",y="G3")
 plt.show()
