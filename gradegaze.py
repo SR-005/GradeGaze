6af8ca0c6cd4e9d0d42098cb9e189418b4071bf3
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-13 19:20:22 +0530

    predictions

--- gradegaze.py
+++ gradegaze.py
@@ -24,3 +24,6 @@ y=df["G3"]
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 lm=LinearRegression()
 lm.fit(X_train,y_train)
+
+prediction=lm.predict(X_test)
+print(prediction)
