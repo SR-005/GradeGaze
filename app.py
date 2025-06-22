c22ff999727e9a69e6a3ed84136354e170185759
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-22 19:09:36 +0530

    result card making

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ def index():
     data=None
     mark=None
     grade=None
+    G1=None
+    G2=None
     if request.method=="POST":
         name=request.form.get("name")
         data={"traveltime":int(request.form.get("traveltime")),
@@ -38,9 +40,10 @@ def index():
         #Rounding off marks for better viewer accuracy
         mark=round(prediction)
         grade=markgrades(mark)
-        #mark,grade=markp(data)
-        print(mark,"    ",grade)
-    return render_template("index.html")
+        G1=int(request.form.get("firstmark"))
+        G2=int(request.form.get("secondmark"))
+        print("Mark:",mark,"\nGrade:",grade)
+    return render_template("index.html",mark=mark,grade=grade,name=name,G1=G1,G2=G2)
 
 if __name__=="__main__":
     app.run(debug=True)
